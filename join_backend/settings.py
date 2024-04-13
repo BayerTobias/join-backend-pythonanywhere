212c9cced15a310b0cd9446cc28e534a8a455d2a
BayerTobias <tbayer2@gmx.de>
2024-04-13 17:01:18 +0200

    CORS_ALLOWED_ORIGINS debugging

--- join_backend/settings.py
+++ join_backend/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-CORS_ALLOWED_ORIGINS = ["*"]
+CORS_ALLOWED_ORIGINS = ["localhost", "127.0.0.1"]
 
 ROOT_URLCONF = "join_backend.urls"
 
